2774367e3e17187c3848ed03a36ffb22039f018a
Isabelle Seal <a2022102518@alunos.estgoh.ipc.pt>
2026-05-13 16:39:12 +0100

    alteracoes token

--- main.py
+++ main.py
@@ -185,24 +185,14 @@ def anomalies():
     cur = conn.cursor()
 
     cur.execute("""
-        SELECT ContadorID, DataHora, KWh_Leitura, DadosAudit
+        SELECT *
         FROM Leituras
-        WHERE 
+        WHERE
             (DadosAudit->>'temperatura')::numeric > 80
             OR (DadosAudit->>'erro_codigo') IS NOT NULL
     """)
-    
-    rows = cur.fetchall()
-    result = []
-    for r in rows:
-        result.append({
-            "contador_id": r[0],
-            "data_hora": r[1].isoformat() if r[1] else None,
-            "kwh": float(r[2]),
-            "dados_audit": r[3]
-        })
 
-    return jsonify(result), OK_CODE
+    return jsonify(cur.fetchall())
 
 
 
